4a17764f9115bd5b31062b798873d836d38c43c2
ousttrue <ousttrue@gmail.com>
2022-02-02 16:05:42 +0900

    mv

--- Editor/Program.cs
+++ Editor/Program.cs
@@ -1,28 +1,160 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Vortice.Win32;
-using static Vortice.Win32.Kernel32;
-using static Vortice.Win32.User32;
 using System.Runtime.CompilerServices;
 using Vortice.Direct3D11;
 using ImGuiNET;
 using Vortice.Direct3D;
+using System.Diagnostics;
+using System.Numerics;
+using Vortice.DXGI;
+using Vortice.Mathematics;
 
 namespace VorticeImGui
 {
-    class MainWindow : AppWindow
+    class App
     {
-        public MainWindow(Win32Window win32window, ID3D11Device device, ID3D11DeviceContext deviceContext) : base(win32window, device, deviceContext)
+        public Win32Window Win32Window;
+
+        ID3D11Device device;
+        ID3D11DeviceContext deviceContext;
+        IDXGISwapChain swapChain;
+        ID3D11Texture2D backBuffer;
+        ID3D11RenderTargetView renderView;
+
+        Format format = Format.R8G8B8A8_UNorm;
+
+        ImGuiRenderer imGuiRenderer;
+        ImGuiInputHandler imguiInputHandler;
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        TimeSpan lastFrameTime;
+
+        IntPtr imGuiContext;
+
+        public App(Win32Window win32window, ID3D11Device device, ID3D11DeviceContext deviceContext)
         {
+            Win32Window = win32window;
+            this.device = device;
+            this.deviceContext = deviceContext;
+
+            imGuiContext = ImGui.CreateContext();
+            ImGui.SetCurrentContext(imGuiContext);
+
+            imGuiRenderer = new ImGuiRenderer(this.device, this.deviceContext);
+            imguiInputHandler = new ImGuiInputHandler(Win32Window.Handle);
+
+            ImGui.GetIO().DisplaySize = new Vector2(Win32Window.Width, Win32Window.Height);
         }
 
-        public override void UpdateImGui()
+        public void Show()
         {
-            base.UpdateImGui();
-            ImGui.ShowDemoWindow();
+            User32.ShowWindow(Win32Window.Handle, ShowWindowCommand.Normal);
         }
+
+        public virtual bool ProcessMessage(uint msg, UIntPtr wParam, IntPtr lParam)
+        {
+            ImGui.SetCurrentContext(imGuiContext);
+            if (imguiInputHandler.ProcessMessage((WindowMessage)msg, wParam, lParam))
+                return true;
+
+            switch ((WindowMessage)msg)
+            {
+                case WindowMessage.Size:
+                    switch ((SizeMessage)wParam)
+                    {
+                        case SizeMessage.SIZE_RESTORED:
+                        case SizeMessage.SIZE_MAXIMIZED:
+                            Win32Window.IsMinimized = false;
+
+                            var lp = (int)lParam;
+                            Win32Window.Width = Utils.Loword(lp);
+                            Win32Window.Height = Utils.Hiword(lp);
+
+                            resize();
+                            break;
+                        case SizeMessage.SIZE_MINIMIZED:
+                            Win32Window.IsMinimized = true;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+            }
+
+            return false;
+        }
+
+        public void UpdateAndDraw()
+        {
+            UpdateImGui();
+            ImGui.ShowDemoWindow();
+            render();
+        }
+
+        void resize()
+        {
+            if (renderView == null)//first show
+            {
+                var dxgiFactory = device.QueryInterface<IDXGIDevice>().GetParent<IDXGIAdapter>().GetParent<IDXGIFactory>();
+
+                var swapchainDesc = new SwapChainDescription()
+                {
+                    BufferCount = 1,
+                    BufferDescription = new ModeDescription(Win32Window.Width, Win32Window.Height, format),
+                    IsWindowed = true,
+                    OutputWindow = Win32Window.Handle,
+                    SampleDescription = new SampleDescription(1, 0),
+                    SwapEffect = SwapEffect.Discard,
+                    Usage = Vortice.DXGI.Usage.RenderTargetOutput
+                };
+
+                swapChain = dxgiFactory.CreateSwapChain(device, swapchainDesc);
+                dxgiFactory.MakeWindowAssociation(Win32Window.Handle, WindowAssociationFlags.IgnoreAll);
+
+                backBuffer = swapChain.GetBuffer<ID3D11Texture2D>(0);
+                renderView = device.CreateRenderTargetView(backBuffer);
+            }
+            else
+            {
+                renderView.Dispose();
+                backBuffer.Dispose();
+
+                swapChain.ResizeBuffers(1, Win32Window.Width, Win32Window.Height, format, SwapChainFlags.None);
+
+                backBuffer = swapChain.GetBuffer<ID3D11Texture2D1>(0);
+                renderView = device.CreateRenderTargetView(backBuffer);
+            }
+        }
+
+        public virtual void UpdateImGui()
+        {
+            ImGui.SetCurrentContext(imGuiContext);
+            var io = ImGui.GetIO();
+
+            var now = stopwatch.Elapsed;
+            var delta = now - lastFrameTime;
+            lastFrameTime = now;
+            io.DeltaTime = (float)delta.TotalSeconds;
+
+            imguiInputHandler.Update();
+
+            ImGui.NewFrame();
+        }
+
+        void render()
+        {
+            ImGui.Render();
+
+            var dc = deviceContext;
+            dc.ClearRenderTargetView(renderView, new Color4(0, 0, 0));
+            dc.OMSetRenderTargets(renderView);
+            dc.RSSetViewport(0, 0, Win32Window.Width, Win32Window.Height);
+
+            imGuiRenderer.Render(ImGui.GetDrawData());
+            DoRender();
+
+            swapChain.Present(0, PresentFlags.None);
+        }
+
+        public virtual void DoRender() { }
     }
 
     class Program
@@ -40,13 +172,13 @@ namespace VorticeImGui
         ID3D11Device device;
         ID3D11DeviceContext deviceContext;
 
-        Dictionary<IntPtr, AppWindow> windows = new Dictionary<IntPtr, AppWindow>();
+        Dictionary<IntPtr, App> windows = new Dictionary<IntPtr, App>();
 
         void Run()
         {
             D3D11.D3D11CreateDevice(null, DriverType.Hardware, DeviceCreationFlags.None, null, out device, out deviceContext);
 
-            var moduleHandle = GetModuleHandle(null);
+            var moduleHandle = Kernel32.GetModuleHandle(null);
 
             var wndClass = new WNDCLASSEX
             {
@@ -54,26 +186,26 @@ namespace VorticeImGui
                 Styles = WindowClassStyles.CS_HREDRAW | WindowClassStyles.CS_VREDRAW | WindowClassStyles.CS_OWNDC,
                 WindowProc = WndProc,
                 InstanceHandle = moduleHandle,
-                CursorHandle = LoadCursor(IntPtr.Zero, SystemCursor.IDC_ARROW),
+                CursorHandle = User32.LoadCursor(IntPtr.Zero, SystemCursor.IDC_ARROW),
                 BackgroundBrushHandle = IntPtr.Zero,
                 IconHandle = IntPtr.Zero,
                 ClassName = "WndClass",
             };
 
-            RegisterClassEx(ref wndClass);
+            User32.RegisterClassEx(ref wndClass);
 
             var win32window = new Win32Window(wndClass.ClassName, "Vortice ImGui", 800, 600);
-            var mainWindow = new MainWindow(win32window, device, deviceContext);
-            windows.Add(mainWindow.Win32Window.Handle, mainWindow);
+            var app = new App(win32window, device, deviceContext);
+            windows.Add(app.Win32Window.Handle, app);
 
-            mainWindow.Show();
+            app.Show();
 
             while (!quitRequested)
             {
-                if (PeekMessage(out var msg, IntPtr.Zero, 0, 0, PM_REMOVE))
+                if (User32.PeekMessage(out var msg, IntPtr.Zero, 0, 0, PM_REMOVE))
                 {
-                    TranslateMessage(ref msg);
-                    DispatchMessage(ref msg);
+                    User32.TranslateMessage(ref msg);
+                    User32.DispatchMessage(ref msg);
 
                     if (msg.Value == (uint)WindowMessage.Quit)
                     {
@@ -89,7 +221,7 @@ namespace VorticeImGui
 
         IntPtr WndProc(IntPtr hWnd, uint msg, UIntPtr wParam, IntPtr lParam)
         {
-            AppWindow window;
+            App window;
             windows.TryGetValue(hWnd, out window);
 
             if (window?.ProcessMessage(msg, wParam, lParam) ?? false)
@@ -98,11 +230,11 @@ namespace VorticeImGui
             switch ((WindowMessage)msg)
             {
                 case WindowMessage.Destroy:
-                    PostQuitMessage(0);
+                    User32.PostQuitMessage(0);
                     break;
             }
 
-            return DefWindowProc(hWnd, msg, wParam, lParam);
+            return User32.DefWindowProc(hWnd, msg, wParam, lParam);
         }
     }
 }
